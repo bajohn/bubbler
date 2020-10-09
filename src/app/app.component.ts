b0e03e1fd0c58ef98b413ded1af7965eba73ef76
Brendan John <bjohn454@gmail.com>
2020-10-08 22:51:03 -0400

    wow, full stack working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Restaurant } from 'src/types/Restaurant';
+import { APIService } from './API.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  restaurants: Restaurant[] = [];
+  constructor(private api: APIService) {
+  }
+
+  ngOnInit() {
+    this.initialize();
+  }
+
+  async initialize() {
+    this.api.OnCreateRestaurantListener.subscribe((event: any) => {
+      console.log(event);
+      const newRestaurant = event.value.data.onCreateRestaurant;
+      this.restaurants = [newRestaurant, ...this.restaurants];
+    });
+
+    const resp = await this.api.ListRestaurants();
+    this.restaurants = resp.items.map(el => {
+      return {
+        city: el.city,
+        description: el.description,
+        id: el.id,
+        name: el.name
+      }
+    });
+  }
+
+  click() {
+    console.log('click');
+    const restaurant: Restaurant = {
+      city: 'Washington',
+      description: 'RIP',
+      name: 'Satellite',
+    };
+    this.api.CreateRestaurant(restaurant)
+  }
+
 
 }
